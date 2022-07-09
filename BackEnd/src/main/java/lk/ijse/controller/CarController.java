1ed4c131cff1b6015529fd08fdaeb523840eb3ea
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-07-09 21:26:39 +0530

    car controller cone with repo and services

--- BackEnd/src/main/java/lk/ijse/controller/CarController.java
+++ BackEnd/src/main/java/lk/ijse/controller/CarController.java
@@ -1,38 +1,43 @@
 package lk.ijse.controller;
 
-import lk.ijse.dto.CustomerDTO;
+import lk.ijse.dto.CarDTO;
+import lk.ijse.service.CarService;
 import lk.ijse.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("car")
 @CrossOrigin
 public class CarController {
+    @Autowired
+    private CarService carService;
+
     @PostMapping
-    ResponseUtil save(@RequestBody CustomerDTO dto) {
-        customerService.save(dto);
-        return new ResponseUtil(200, "Customer Saved Successfully", dto);
+    ResponseUtil save(@RequestBody CarDTO dto) {
+        carService.save(dto);
+        return new ResponseUtil(200, "Car Saved Successfully", dto);
     }
 
     @PutMapping
-    ResponseUtil update(@RequestBody CustomerDTO dto) {
-        customerService.update(dto);
-        return new ResponseUtil(200, "Customer Updated Successfully", dto);
+    ResponseUtil update(@RequestBody CarDTO dto) {
+        carService.update(dto);
+        return new ResponseUtil(200, "Car Updated Successfully", dto);
     }
 
     @DeleteMapping(params = {"nic"})
     ResponseUtil delete(@RequestParam String nic) {
-        customerService.delete(nic);
-        return new ResponseUtil(200, "Customer delete Successfully", null);
+        carService.delete(nic);
+        return new ResponseUtil(200, "Car delete Successfully", null);
     }
 
     @GetMapping(path = "getAll")
     ResponseUtil getAll() {
-        return new ResponseUtil(200, "Data Fetched Successfully", customerService.getAll());
+        return new ResponseUtil(200, "Data Fetched Successfully", carService.getAll());
     }
 
     @GetMapping(path = "search", params = {"nic"})
     ResponseUtil search(@RequestParam String nic) {
-        return new ResponseUtil(200, "Customer Searched Successfully", customerService.search(nic));
+        return new ResponseUtil(200, "Car Searched Successfully", carService.search(nic));
     }
 }
