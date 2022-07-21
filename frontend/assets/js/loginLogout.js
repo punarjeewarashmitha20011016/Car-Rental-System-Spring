6c739310dfcef996265c185d6f65ae4925649385
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-07-21 18:43:55 +0530

    committed frontend again due to an error occured in the project

---
+++ frontend/assets/js/loginLogout.js
@@ -0,0 +1,23 @@
+var signUpBtn = document.getElementById("signupBtn");
+var logoutBtn = document.getElementById("logoutBtn");
+
+logoutBtn.addEventListener("mouseover", function (event) {
+    signUpBtn.style.right = '150px';
+}, false)
+
+logoutBtn.addEventListener("mouseleave", function (event) {
+    signUpBtn.style.right = '70px';
+}, false)
+
+
+//this.is for signup and logout btns in signup section
+var logoutBtnInSignup = $('#logoutBtnInSignup');
+var signupForAdmin = $('#signupForAdminBtn');
+
+logoutBtnInSignup.mouseover(function () {
+    signupForAdmin.css('right', '15%');
+})
+
+logoutBtnInSignup.mouseleave(function () {
+    signupForAdmin.css('right', '10%');
+})
